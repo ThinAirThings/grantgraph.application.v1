2bd6d6323fdd05f7718bda46f3ca5968c812ebd2
irishcarbomb777 <danielclannan@gmail.com>
2024-01-07 09:35:23 -0500

    test deploy

--- app/login/_page.tsx
+++ app/login/_page.tsx
@@ -1,22 +1,19 @@
 
 import { redirect } from "next/navigation"
-import { auth } from "../lib/auth"
 import { Card, Flex} from "@radix-ui/themes"
 import Image from "next/image"
-import { LoginForm } from "./LoginForm";
 
 
 export default async function () {
-    const session = await auth()
-    if (session?.user) {
-        redirect('/dashboard')
-    }
+    // const session = await auth()
+    // if (session?.user) {
+    //     redirect('/dashboard')
+    // }
     return (
         <Flex justify={'center'} align='center' className="h-screen">
             <Card className="w-[392px] p-10">
                 <Flex className="flex flex-col justify-start items-center gap-5">
                     <Image src="/assets/logos.blueprint/blueprint-logo-fullText.svg" width={250} height={100} alt="logo" />
-                    <LoginForm/>
                 </Flex>
             </Card>
         </Flex>
